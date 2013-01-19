33d8bf0ce7382c75140092d1519d018e585f3de8
Lisa J. Lovchik <exexpat2@gmail.com>
2013-01-18 22:07:30 -0800

    First round of bugfixes

--- php/maps_feed.php
+++ php/maps_feed.php
@@ -7,8 +7,9 @@
  *  '-' `-`-'`-`--'|`-'  `-'  `-' `-`--' v0.2
  
  *  Copyright (C) 2012-2013 Open Technology Institute <tidepools@opentechinstitute.org>
- *	Lead: Jonathan Baldwin
- *	This file is part of Tidepools <http://www.tidepools.co>
+ *	    Lead: Jonathan Baldwin
+ *      Contributors: Lisa J. Lovchik
+ *	    This file is part of Tidepools <http://www.tidepools.co>
 
  *  Tidepools is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -24,34 +25,41 @@
  *  along with Tidepools.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-	$maps = $_POST['data'];
+/**
+ * maps_feed.php
+ * 
+ *     Called from:
+ *         js/tidepools_conf.js
+ *
+ *     Calls:
+ *         none
+ */
 
-	stripslashes($maps); 
-	
-	$counter = 1;
-	
 
-	echo '<div style=" width:323%; height:54px; background-color:#f2edea; position:absolute; padding-top:12; padding-left:12; ">   ';
+$maps = (isset($_POST['data']) ? $_POST['data'] : null);
+stripslashes($maps); 
 
+$counter = 1;
 
-foreach($maps as $i){
+echo '<div style=" width:323%; height:54px; background-color:#f2edea; position:absolute; padding-top:12; padding-left:12; ">   ';
 
-	$idVal = "'".$i['_id']['$id']."'";
-	$selectVal = "'map".$counter."'";
-	
-		
-	echo '<div id="map'.$counter.'" style="width:95px; height:35px; position:relative; float:left;" onclick="reBoundFilterMap('.$idVal.','.$selectVal.');">';
-	echo '<img style="float:left; position:relative; display:inline-block; cursor:pointer;" src="images/icon_map.png"/>';
-	echo "<p style='font-size:11px;'>".$counter." ".$i['name']."</p>     ";  //plug into CSS here...
-	
+if ($maps != null){
+
+    foreach($maps as $i){
+
+        $idVal = "'".$i['_id']['$id']."'";
+        $selectVal = "'map".$counter."'";
+            
+        echo '<div id="map'.$counter.'" style="width:95px; height:35px; position:relative; float:left;" onclick="reBoundFilterMap('.$idVal.','.$selectVal.');">';
+        echo '<img style="float:left; position:relative; display:inline-block; cursor:pointer;" src="images/icon_map.png"/>';
+        echo "<p style='font-size:11px;'>".$counter." ".$i['name']."</p>     ";  //plug into CSS here...
+        
+        echo "</div>";
+                    
+        $counter++;
+    }
+
 	echo "</div>";
-				
-	$counter++;
-	
-	}
-
-	echo "</div>";
+}
 
 ?>
-	
-
