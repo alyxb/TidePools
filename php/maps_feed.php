bb4173c6caea908f2544b1a97c253d454c571490
Lisa J. Lovchik <exexpat2@gmail.com>
2013-01-18 22:26:33 -0800

    Minor formatting tweak

--- php/maps_feed.php
+++ php/maps_feed.php
@@ -7,9 +7,9 @@
  *  '-' `-`-'`-`--'|`-'  `-'  `-' `-`--' v0.2
  
  *  Copyright (C) 2012-2013 Open Technology Institute <tidepools@opentechinstitute.org>
- *	    Lead: Jonathan Baldwin
+ *	Lead: Jonathan Baldwin
  *      Contributors: Lisa J. Lovchik
- *	    This file is part of Tidepools <http://www.tidepools.co>
+ *	This file is part of Tidepools <http://www.tidepools.co>
 
  *  Tidepools is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -59,7 +59,7 @@ if ($maps != null){
         $counter++;
     }
 
-	echo "</div>";
+    echo "</div>";
 }
 
 ?>
